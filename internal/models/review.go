11f9873e3d7515f567b029ceffac9387d19dc415
fareeza05 <fareezarahman123@gmail.com>
2025-08-03 13:51:34 -0400

    add input validation

--- internal/models/review.go
+++ internal/models/review.go
@@ -6,30 +6,30 @@ package models
 type AnalyzeRequest struct {
 	//json:"field" -> tells Go how to map JSON keys to go fields
 	//binding: "required" -> Used by Gin to validate that field must be present
-	Code     string `json:"code" binding:"required"`
-	Language string `json:"language" binding:"required"`
+	Code     string `json:"code" binding:"required, min=10"`
+	Language string `json:"language" binding:"required, oneof=go python javascript"`
 }
 
 // ReviewResponse = JSON response returned by API
 // Defines what backend sends back to client for a GET request
 // POST /analyze response (202 Accepted)
 type AnalyzeResponse struct {
-	ID      string `json:"id"` //UUID
-	Message string `json:"message"`
+	ID      string `json:"id" binding:"required, uuid"` //UUID - required
+	Message string `json:"message" binding:"required"`  //required
 }
 
 // GET /results/{id} response
 type AnalysisResult struct {
-	ID          string   `json:"id"`
-	Summary     string   `json:"summary"`
-	Issues      []string `json:"issues"`
-	Suggestions []string `json:"suggestions"`
+	ID          string   `json:"id" binding:"required, uuid"`
+	Summary     string   `json:"summary" binding:"required"`
+	Issues      []string `json:"issues" binding:"required"`
+	Suggestions []string `json:"suggestions" binding:"required"`
 }
 
 // GET /status/{id} response
 type AnalysisStatus struct {
-	ID     string `json:"id"`
-	Status string `json:"status"`
+	ID     string `json:"id" binding:"required, uuid"`
+	Status string `json:"status" binding:"required"`
 }
 
 // Generic Error response
